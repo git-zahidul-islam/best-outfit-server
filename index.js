96a848ba1180b736b30fe390754009c1c5acec87
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-08-15 21:23:46 +0600

    remove: some unnessery code clean

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
@@ -28,29 +27,6 @@ async function run() {
         const userCollection = client.db('best-outfit').collection('users')
         const productCollection = client.db('best-outfit').collection('products')
 
-        // jwt token api making related
-        app.post('/jwt', async (req, res) => {
-            const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
-            // console.log("the token is ",token);
-            res.send({ token })
-        })
-        // middleware
-        const verifyToken = (req, res, next) => {
-            console.log("inserted token", req.headers.authorization);
-            if (!req.headers.authorization) {
-                return res.status(401).send({ message: 'Unauthorized access' })
-            }
-            const token = req.headers.authorization.split(' ')[1]
-            jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
-                if (err) {
-                    return res.status(401).send({ message: 'Unauthorized access' })
-                }
-                req.decoded = decoded;
-                next()
-            })
-        }
-
         app.post('/users', async (req, res) => {
             const users = req.body;
             const {email, name} = users;
@@ -63,16 +39,6 @@ async function run() {
             const result = await userCollection.insertOne(users);
             res.send(result)
         })
-
-        // app.get('/products',async(req,res)=>{
-        //     try {
-        //         const products = await productsCollection.find().toArray();
-        //         res.send(products)
-        //     } catch (error) {
-        //         res.status(500).send({ message: 'Error fetching products', error });
-        //     }
-        // })
-
 
         
     app.get("/products", async (req, res) => {
